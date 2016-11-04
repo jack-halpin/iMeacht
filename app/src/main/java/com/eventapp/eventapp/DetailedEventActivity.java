503a1201b8b24ba3f4f721119f1c96b37c845f36
Conor Devoy <conor.devoy@ucdconnect.ie>
2016-11-04 16:08:41 +0000

    added detailed event activity

---
+++ app/src/main/java/com/eventapp/eventapp/DetailedEventActivity.java
@@ -0,0 +1,13 @@
+package com.eventapp.eventapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailedEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailed_event);
+    }
+}
