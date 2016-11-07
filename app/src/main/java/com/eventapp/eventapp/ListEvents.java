2424877360711545200a44348afa93f4c6c180ab
Jack Halpin <jhalpin92@gmail.com>
2016-11-07 17:54:27 +0000

    baisc activity now displays list of event titles

--- app/src/main/java/com/eventapp/eventapp/ListEvents.java
+++ app/src/main/java/com/eventapp/eventapp/ListEvents.java
@@ -6,8 +6,19 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.Time;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -20,130 +31,92 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class ListEvents extends AppCompatActivity {
+
+    //This is declared in the class so it can be accessed by the inner public class
+    private ArrayAdapter<String> eventLists;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_events);
+        // Get ListView object from xml
+        ListView listView = (ListView) findViewById(R.id.activity_list);
 
+        // Defined Array values to show in ListView
+
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        eventLists = new ArrayAdapter<String>(this, R.layout.event_list_entry, R.id.entry_text, new ArrayList<String>());
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(eventLists);
+
+
+
+
+//        String[] tests = new String[3];
+//        tests[0] = "test";
+//        tests[1] = "test";
+//        tests[2] = "test";
+//
+//
+//
+//        eventLists =
+//                new ArrayAdapter<String>(
+//                        this, // The current context (this activity)
+//                        R.layout.event_list_entry, // The name of the layout ID.
+//                        R.id.entry_text, // The ID of the textview to populate.
+//                        new ArrayList<String>());
+//
+//        ListView listView = (ListView) findViewById(R.id.activity_list);
+//        listView.setAdapter(eventLists);
+        FetchEventInfo fetch = new FetchEventInfo();
+        fetch.execute("hi");
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         //We can use this to inflate the menu at the top right of the screen
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu, menu);
+        inflater.inflate(R.menu.listings_menu, menu);
         return true;
     }
 
-
-    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
-
-        private final String LOG_TAG = FetchWeatherTask.class.getSimpleName();
-
-
-        /* The date/time conversion code is going to be moved outside the asynctask later,
-        * so for convenience we're breaking it out into its own method now.
-        */
-        private String getReadableDateString(long time){
-            // Because the API returns a unix timestamp (measured in seconds),
-            // it must be converted to milliseconds in order to be converted to valid date.
-            SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
-            return shortenedDateFormat.format(time);
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.get_info){
+            getEventInfo();
+            return true;
         }
+        return super.onOptionsItemSelected(item);
 
-        /**
-         * Prepare the weather high/lows for presentation.
-         */
-        private String formatHighLows(double high, double low) {
-            // For presentation, assume the user doesn't care about tenths of a degree.
-            long roundedHigh = Math.round(high);
-            long roundedLow = Math.round(low);
+    }
 
-            String highLowStr = roundedHigh + "/" + roundedLow;
-            return highLowStr;
-        }
+    public void getEventInfo(){
+        //Here we will create a new async FetchEventInfo class and tell it to do it's stuff
+        FetchEventInfo fetch = new FetchEventInfo();
+        fetch.execute("test");
+    }
 
 
-        /**
-         * Take the String representing the complete forecast in JSON Format and
-         * pull out the data we need to construct the Strings needed for the wireframes.
-         *
-         * Fortunately parsing is easy:  constructor takes the JSON string and converts it
-         * into an Object hierarchy for us.
-         */
-        private String[] getWeatherDataFromJson(String forecastJsonStr, int numDays)
-                throws JSONException {
 
-            // These are the names of the JSON objects that need to be extracted.
-            final String OWM_LIST = "list";
-            final String OWM_WEATHER = "weather";
-            final String OWM_TEMPERATURE = "temp";
-            final String OWM_MAX = "max";
-            final String OWM_MIN = "min";
-            final String OWM_DESCRIPTION = "main";
 
-            JSONObject forecastJson = new JSONObject(forecastJsonStr);
-            JSONArray weatherArray = forecastJson.getJSONArray(OWM_LIST);
+    //The class represents an asynchronous task to be carried out when the activity is loaded
+    //In here we must define the URL, perform a JSON request with the parameters and parse it.
+    //objects that have been defined in the parent class can be accessed here!
+    public class FetchEventInfo extends AsyncTask<String, Void, String[]> {
 
-            // OWM returns daily forecasts based upon the local time of the city that is being
-            // asked for, which means that we need to know the GMT offset to translate this data
-            // properly.
-
-            // Since this data is also sent in-order and the first day is always the
-            // current day, we're going to take advantage of that to get a nice
-            // normalized UTC date for all of our weather.
-
-//            Time dayTime = new Time();
-//            dayTime.setToNow();
-
-            // we start at the day returned by local time. Otherwise this is a mess.
-//            int julianStartDay = Time.getJulianDay(System.currentTimeMillis(), dayTime.gmtoff);
-
-            // now we work exclusively in UTC
-//            dayTime = new Time();
-
-            String[] resultStrs = new String[numDays];
-            for(int i = 0; i < weatherArray.length(); i++) {
-                // For now, using the format "Day, description, hi/low"
-                String day;
-                String description;
-                String highAndLow;
-
-                // Get the JSON object representing the day
-                JSONObject dayForecast = weatherArray.getJSONObject(i);
-
-                // The date/time is returned as a long.  We need to convert that
-                // into something human-readable, since most people won't read "1400356800" as
-                // "this saturday".
-                long dateTime;
-                // Cheating to convert this to UTC time, which is what we want anyhow
-                dateTime = dayTime.setJulianDay(julianStartDay+i);
-                day = getReadableDateString(dateTime);
-
-                // description is in a child array called "weather", which is 1 element long.
-                JSONObject weatherObject = dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
-                description = weatherObject.getString(OWM_DESCRIPTION);
-
-                // Temperatures are in a child object called "temp".  Try not to name variables
-                // "temp" when working with temperature.  It confuses everybody.
-                JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
-                double high = temperatureObject.getDouble(OWM_MAX);
-                double low = temperatureObject.getDouble(OWM_MIN);
-
-                highAndLow = formatHighLows(high, low);
-                resultStrs[i] = day + " - " + description + " - " + highAndLow;
-            }
-
-            //for (String s : resultStrs) {
-            //    Log.v(LOG_TAG, "Forecast entry: " + s);
-            //}
-            return resultStrs;
-
-        }
-
+        private final String LOG_TAG = FetchEventInfo.class.getSimpleName();
 
         @Override
         protected String[] doInBackground(String... params) {
@@ -159,36 +132,17 @@ public class ListEvents extends AppCompatActivity {
             BufferedReader reader = null;
 
             // Will contain the raw JSON response as a string.
-            String forecastJsonStr = null;
+            String eventJsonStr = null;
 
-            String format = "json";
-            String units = "metric";
-            int numDays = 10;
 
-            // You will need to replace this key with your own one. To get a key
-            // go to http://openweathermap.org/api and sign up.
-            String key = "19b104f014c41d11939f615df3a80edf";
+
 
             try {
-                // Construct the URL for the OpenWeatherMap query
-                // Possible parameters are avaiable at OWM's forecast API page, at
-                // http://openweathermap.org/API#forecast
 
-                final String FORECAST_BASE_URL =
-                        "http://api.openweathermap.org/data/2.5/forecast/daily?";
-                final String QUERY_PARAM = "id";
-                final String FORMAT_PARAM = "mode";
-                final String UNITS_PARAM = "units";
-                final String DAYS_PARAM = "cnt";
-                final String KEY_PARAM = "APPID";
+                //Querying test URL:
+                final String testurl = "http://api.eventful.com/json/events/search?app_key=p3tDfpd3dKGs2HBD&location=San%20Diego";
+                Uri builtUri = Uri.parse(testurl);
 
-                Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
-                        .appendQueryParameter(QUERY_PARAM, params[0])
-                        .appendQueryParameter(FORMAT_PARAM, format)
-                        .appendQueryParameter(UNITS_PARAM, units)
-                        .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
-                        .appendQueryParameter(KEY_PARAM, key)
-                        .build();
 
                 URL url = new URL(builtUri.toString());
 
@@ -196,28 +150,24 @@ public class ListEvents extends AppCompatActivity {
 
                 //URL url = new URL("http://http://api.openweathermap.org/data/2.5/forecast/daily?id=524901&mode=json&units=metric&ctn=7");
 
-                // Create the request to OpenWeatherMap, and open the connection
+                // Create the request to Eventful, and open the connection
                 urlConnection = (HttpURLConnection) url.openConnection();
                 urlConnection.setRequestMethod("GET");
                 urlConnection.connect();
 
                 // Read the input stream into a String
                 InputStream inputStream = urlConnection.getInputStream();
-                forecastJsonStr = readStream(inputStream);
+                eventJsonStr = readStream(inputStream);
 
-                //forecastJsonStr="{\"city\":{\"id\":7778677,\"name\":\"Dublin City\",\"coord\":{\"lon\":-6.24922,\"lat\":53.355122},\"country\":\"IE\",\"population\":0},\"cod\":\"200\",\"message\":0.0199,\"cnt\":7,\"list\":[{\"dt\":1444564800,\"temp\":{\"day\":11.14,\"min\":10.77,\"max\":12.46,\"night\":12.46,\"eve\":11.96,\"morn\":10.77},\"pressure\":1025.62,\"humidity\":100,\"weather\":[{\"id\":803,\"main\":\"Clouds\",\"description\":\"broken clouds\",\"icon\":\"04d\"}],\"speed\":4.35,\"deg\":100,\"clouds\":80},{\"dt\":1444651200,\"temp\":{\"day\":12.05,\"min\":11.13,\"max\":12.85,\"night\":12.04,\"eve\":12.63,\"morn\":11.25},\"pressure\":1032.62,\"humidity\":100,\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"sky is clear\",\"icon\":\"02d\"}],\"speed\":8.61,\"deg\":358,\"clouds\":8},{\"dt\":1444737600,\"temp\":{\"day\":12.37,\"min\":11.97,\"max\":12.39,\"night\":12.02,\"eve\":12.1,\"morn\":12.24},\"pressure\":1036.05,\"humidity\":100,\"weather\":[{\"id\":803,\"main\":\"Clouds\",\"description\":\"broken clouds\",\"icon\":\"04d\"}],\"speed\":5.61,\"deg\":50,\"clouds\":64},{\"dt\":1444824000,\"temp\":{\"day\":11.23,\"min\":8.95,\"max\":11.23,\"night\":9.86,\"eve\":10.99,\"morn\":8.95},\"pressure\":1036.52,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":6.35,\"deg\":131,\"clouds\":0},{\"dt\":1444910400,\"temp\":{\"day\":12.47,\"min\":8.78,\"max\":12.47,\"night\":10.21,\"eve\":11.44,\"morn\":8.78},\"pressure\":1036.04,\"humidity\":0,\"weather\":[{\"id\":800,\"main\":\"Clear\",\"description\":\"sky is clear\",\"icon\":\"01d\"}],\"speed\":5.45,\"deg\":50,\"clouds\":0},{\"dt\":1444996800,\"temp\":{\"day\":12.96,\"min\":9.26,\"max\":12.96,\"night\":9.26,\"eve\":11.74,\"morn\":9.78},\"pressure\":1033.93,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":3.5,\"deg\":7,\"clouds\":2},{\"dt\":1445083200,\"temp\":{\"day\":12.68,\"min\":8.32,\"max\":12.68,\"night\":9.12,\"eve\":11.59,\"morn\":8.32},\"pressure\":1033.53,\"humidity\":0,\"weather\":[{\"id\":500,\"main\":\"Rain\",\"description\":\"light rain\",\"icon\":\"10d\"}],\"speed\":1.86,\"deg\":342,\"clouds\":3}]}";
+                //read
+                Log.v(LOG_TAG, "Event JSON String: " + eventJsonStr);
 
-                Log.v(LOG_TAG, "Forecast JSON String: " + forecastJsonStr);
-
-            }
-            catch (IOException e) {
+            } catch (IOException e) {
                 Log.e(LOG_TAG, "Error ", e);
                 // If the code didn't successfully get the weather data, there's no point in attemping
                 // to parse it.
                 return null;
-            }
-
-            finally {
+            } finally {
                 if (urlConnection != null) {
                     urlConnection.disconnect();
                 }
@@ -230,17 +180,32 @@ public class ListEvents extends AppCompatActivity {
                 }
             }
 
-            try {
-                return getWeatherDataFromJson(forecastJsonStr, numDays);
-            } catch (JSONException e) {
+            try{
+                return getEventInfoFromJson(eventJsonStr);
+            }
+            catch(JSONException e){
                 Log.e(LOG_TAG, e.getMessage(), e);
                 e.printStackTrace();
             }
+
 
             // This will only happen if there was an error getting or parsing the forecast.
             return null;
         }
 
+
+        public String[] getEventInfoFromJson(String eventInfo) throws JSONException{
+            JSONObject eventJson = new JSONObject(eventInfo);
+            JSONObject eventlistings = eventJson.getJSONObject("events");
+            JSONArray events = eventlistings.getJSONArray("event");
+            String[] resultStrs = new String[events.length()];
+            for(int i = 0; i < events.length(); i++) {
+                JSONObject currEvent = events.getJSONObject(i);
+                Log.v(LOG_TAG, "Current Event Name: " + currEvent.getString("title"));
+                resultStrs[i] = currEvent.getString("title");
+            }
+            return resultStrs;
+        }
 
         private String readStream(InputStream in) {
             BufferedReader reader = null;
@@ -267,16 +232,19 @@ public class ListEvents extends AppCompatActivity {
 
         @Override
         protected void onPostExecute(String[] results) {
-            //super.onPostExecute(strings);
+            Log.e(LOG_TAG, "Got here");
 
             if (results != null) {
-                mForecastAdapter.clear();
+                eventLists.clear();
                 //mForecastAdapter.addAll(result);
-                for (String dayForeCastStr: results){
-                    mForecastAdapter.add(dayForeCastStr);
-                }
-            }
-        }
+                for (int i = 0; i < results.length; i++) {
+                    Log.e(LOG_TAG, results[i].toString());
 
+                    eventLists.add(results[i].toString());
+                }
+                Log.e(LOG_TAG, "First Item: " + eventLists.getItem(0));
+            }
+
+        }
     }
 }
