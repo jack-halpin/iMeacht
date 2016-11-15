18fc8a70c07c5f11b885664eb6c4f6b416cdfc5b
Devin Stacey <devin.stacey@ucdconnect.ie>
2016-11-15 13:06:35 +0000

    Added details

--- app/src/main/java/com/eventapp/eventapp/MapsActivity.java
+++ app/src/main/java/com/eventapp/eventapp/MapsActivity.java
@@ -9,14 +9,17 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private ArrayList<MapDetails> locations;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        locations = (ArrayList<MapDetails>) getIntent().getExtras().getSerializable("map");
         setContentView(R.layout.activity_maps);
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -27,10 +30,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        for (MapDetails loc : locations) {
+            LatLng latlngs = new LatLng(loc.getLat(), loc.getLng());
+            mMap.addMarker(new MarkerOptions().position(latlngs)
+                .title(loc.getTitle()).snippet(loc.getSnippet()));
+        }
         LatLng latlngs = new LatLng(0, 0);
-        mMap.addMarker(new MarkerOptions().position(latlngs).title("Marker").snippet("Something"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(latlngs));
-
     }
 }
