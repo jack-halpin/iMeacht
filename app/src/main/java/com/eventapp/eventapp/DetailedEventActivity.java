35df5c71627bf0f11e8197e22d5a5d121b7b523c
Patrick Harney <patrick.harney@ucdconnect.ie>
2016-11-16 18:12:38 +0000

    calendar Share

--- app/src/main/java/com/eventapp/eventapp/DetailedEventActivity.java
+++ app/src/main/java/com/eventapp/eventapp/DetailedEventActivity.java
@@ -1,9 +1,14 @@
 package com.eventapp.eventapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+
+import java.util.Calendar;
 
 public class DetailedEventActivity extends AppCompatActivity {
 
@@ -18,5 +23,24 @@ public class DetailedEventActivity extends AppCompatActivity {
 
         TextView v = (TextView) findViewById(R.id.textView2);
         v.setText(a);
+    }
+
+    /** Called when the user clicks the Send button */
+    public void addCalendar(View view) {
+        Calendar beginTime = Calendar.getInstance();
+        beginTime.set(2016, 10, 16, 18, 30);
+        Calendar endTime = Calendar.getInstance();
+        endTime.set(2016, 10, 16, 20, 30);
+        Intent intent = new Intent(Intent.ACTION_INSERT)
+                .setData(CalendarContract.Events.CONTENT_URI)
+                .putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME, beginTime.getTimeInMillis())
+                .putExtra(CalendarContract.EXTRA_EVENT_END_TIME, endTime.getTimeInMillis())
+                .putExtra(CalendarContract.Events.TITLE, "Android Swotting")
+                .putExtra(CalendarContract.Events.DESCRIPTION, "This is when I sit around trying to" +
+                        "code in Android")
+                .putExtra(CalendarContract.Events.EVENT_LOCATION, "Boardroom Science Building")
+                .putExtra(CalendarContract.Events.AVAILABILITY, CalendarContract.Events.AVAILABILITY_BUSY)
+                .putExtra(Intent.EXTRA_EMAIL, "patrick.harney@gmail.com, patrick.harney@ucdconnect.ie");
+        startActivity(intent);
     }
 }
