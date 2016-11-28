0b8c12fb9dadb0c37a73fe317e0a8c8748eb72c6
Devin Stacey <devin.stacey@ucdconnect.ie>
2016-11-28 16:36:50 +0000

    Changed tut a bit

--- app/src/main/java/com/eventapp/eventapp/SplashActivity.java
+++ app/src/main/java/com/eventapp/eventapp/SplashActivity.java
@@ -11,23 +11,9 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
-        if (pref.getBoolean("firstTimeBoot", true)) {
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.putExtra("firstTime", true);
-            startActivity(intent);
-            SharedPreferences.Editor editor = pref.edit();
-            editor.putBoolean("firstTimeBoot", false);
-            editor.apply();
-        } else {
-            Intent intent = new Intent(this, MainActivity.class);
-            intent.putExtra("firstTime", false);
-            startActivity(intent);
-        }
-        finish();
 
-//        Intent intent = new Intent(this, MainActivity.class);
-//        startActivity(intent);
-//        finish();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
