c0c66e39551688ddf38dd4a4362246b5c1871577
Devin Stacey <devin.stacey@ucdconnect.ie>
2016-11-29 16:14:30 +0000

    Added PreferencesActivity

---
+++ app/src/main/java/com/eventapp/eventapp/EventPreferencesActivity.java
@@ -0,0 +1,29 @@
+package com.eventapp.eventapp;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+
+/**
+ * Created by devin on 29/11/16.
+ */
+
+public class EventPreferencesActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+    }
+
+    public static class MyPreferenceFragment extends PreferenceFragment {
+
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+
+    }
+
+}
