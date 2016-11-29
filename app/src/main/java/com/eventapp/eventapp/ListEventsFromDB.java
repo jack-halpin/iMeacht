ccc9400ba720c065e33d2ca9b3409ef75f81ae80
Jack Halpin <jhalpin92@gmail.com>
2016-11-29 15:48:52 +0000

    added fragment to savedEventsActivity

---
+++ app/src/main/java/com/eventapp/eventapp/ListEventsFromDB.java
@@ -0,0 +1,115 @@
+package com.eventapp.eventapp;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * Created by Jack on 29/11/2016.
+ */
+
+public class ListEventsFromDB  extends Fragment {
+
+    private EventAdapter eventLists;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.e("onCreateView: ", "Called");
+        View rootView = inflater.inflate(R.layout.activity_list_events, container, false);
+
+        // Get ListView object from xml
+        ListView listView = (ListView) rootView.findViewById(R.id.activity_list);
+
+        // Defined Array values to show in ListView
+
+
+        // Define a new Adapter
+        // First parameter - Context
+        // Second parameter - Layout for the row
+        // Third parameter - ID of the TextView to which the data is written
+        // Forth - the Array of data
+
+        eventLists = new EventAdapter(getActivity(), R.layout.event_list_entry, new ArrayList<EventListing>());
+
+
+        // Assign adapter to ListView
+        listView.setAdapter(eventLists);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                Log.e("Logger:", Integer.toString(position));
+
+                Context context = getActivity();
+                EventListing a = eventLists.getItem(position);
+                Intent intent = new Intent(getActivity(), DetailedEventActivity.class);
+//                                                intent.putExtra("EVENT_ID", a.getId());
+                intent.putExtra("EVENT_OBJ", a);
+                startActivity(intent);
+
+            }
+        });
+        return rootView;
+    }
+
+
+    public void getEventInfo(){
+        if (eventLists.isEmpty() == true){
+            Log.e("E", "is empty");
+            FetchEventInfoFromDB fetch = new ListEvents.FetchEventInfo();
+            fetch.execute("test");
+        }
+        //Here we will create a new async FetchEventInfo class and tell it to do it's stuff
+        Log.e("getEventInfo:", "called");
+
+    }
+
+    @Override
+    public void onStart(){
+        Log.e("onStart", "called");
+        super.onStart();
+        getEventInfo();
+    }
+
+
+
+
+    public class FetchEventInfoFromDB extends AsyncTask<Void, Void, EventListing[]> {
+
+        private final String LOG_TAG = ListEvents.FetchEventInfo.class.getSimpleName();
+
+        @Override
+        protected EventListing[] doInBackground(Void... params) {
+            //Read in the event objects from the database
+
+        }
+
+
+
+        @Override
+        protected void onPostExecute(EventListing[] results) {
+            //For each object in the array, add them ot eventLists
+        }
+}
